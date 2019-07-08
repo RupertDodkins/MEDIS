8f5a8f492e11777a74789cf9c2a248d7b7c1adc4
rdodkins <rupertdodkins@gmail.com>
2019-07-08 16:35:21 -0700

    mod-barebones now creates two outputs - one without a coronagraph and one with. fields.h5 is saved in subfolders with minimally descriptive names. next step: headers in h5s

--- examples/modified_barebones.py
+++ examples/modified_barebones.py
@@ -14,7 +14,6 @@ tp.use_spiders =False #spiders off/on
 tp.use_ao = True #AO off/on
 tp.piston_error = False #piston error off/on
 tp.use_coron = False #coronagraph off/on
-tp.occulter_type = 'None' #occulter type - vortex, none, gaussian
 tp.detector = 'ideal'
 tp.check_args()
 
@@ -25,8 +24,12 @@ sp.num_processes = 1
 sp.save_locs = np.array(['add_atmos', 'coronagraph'])
 sp.return_E = True
 
-iop.update("mod_barebones/")
+occulter_types = ['None', 'Vortex']
 
-if __name__ == '__main__':
-    save_E_fields = gpd.run_medis(plot=False)
-    print(save_E_fields.shape)  # ap.numframes x len(sp.save_locs) x (1 star + # of companions) x ap.nwsamp x ap.grid_size x ap.grid_size
+for i in range(2):
+    iop.update("mod_barebones/coron_%s" % occulter_types[i])
+    tp.occulter_type = occulter_types[i]  # occulter type - vortex, none, gaussian
+
+    if __name__ == '__main__':
+        save_E_fields = gpd.run_medis(plot=False)
+        print(save_E_fields.shape)  # ap.numframes x len(sp.save_locs) x (1 star + # of companions) x ap.nwsamp x ap.grid_size x ap.grid_size
